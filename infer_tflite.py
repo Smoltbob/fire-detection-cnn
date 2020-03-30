35f811b05d719e365ee0ca500a788f93cd9b7345
Jules Simon <jules.simon@protonmail.com>
2020-03-30 16:04:41 -0400

    light comments

--- infer_tflite.py
+++ infer_tflite.py
@@ -1,9 +1,9 @@
 from model import *
 import os
 
+# load the model
 tflife_model = tf.lite.Interpreter(model_path="models/firenet.tflite")
 tflife_model.allocate_tensors()
-
 tflife_input_details = tflife_model.get_input_details()
 tflife_output_details = tflife_model.get_output_details()
 
@@ -12,7 +12,7 @@ tflife_output_details = tflife_model.get_output_details()
 rows = 224
 cols = 224
 
-frame = cv2.imread("images/chile.jpg")
+frame = cv2.imread("images/protest.jpg")
 # re-size image to network input size and perform prediction
 small_frame = cv2.resize(frame, (rows, cols), cv2.INTER_AREA)
 tflife_input_data = np.reshape(np.float32(small_frame), (1, 224, 224, 3))
@@ -20,6 +20,7 @@ tflife_model.set_tensor(tflife_input_details[0]['index'], tflife_input_data)
 
 tflife_model.invoke()
 
+# compute output
 output = tflife_model.get_tensor(tflife_output_details[0]['index'])
 output = output[0][0]
 
